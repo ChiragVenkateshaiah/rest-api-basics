27531c2968bda047fdd23902a4c031f3c06c6b5b
ChiragVenkateshaiah <chiragvenkateshaiah@gmail.com>
2025-12-17 15:29:42 +0530

    style: format code with black and isort

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 db_url = "postgresql://postgres:admin@localhost:5432/my_db"
 engine = create_engine(db_url)

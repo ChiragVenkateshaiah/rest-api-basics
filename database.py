773f1e822c1e817543dd068cbf887f7931d1e08d
ChiragVenkateshaiah <chiragvenkateshaiah@gmail.com>
2025-12-17 14:58:01 +0530

    REST APIs basics builds

---
+++ database.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+db_url = "postgresql://postgres:admin@localhost:5432/my_db"
+engine = create_engine(db_url)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
